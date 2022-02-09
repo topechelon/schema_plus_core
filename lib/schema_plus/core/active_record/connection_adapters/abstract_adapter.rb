f96988583f5004e8686fad724399f444b94fce28
Ryan Vall <rvall@topechelon.com>
2022-02-09 13:28:48 -0500

    attempt to fix SchemaCreation error

--- lib/schema_plus/core/active_record/connection_adapters/abstract_adapter.rb
+++ lib/schema_plus/core/active_record/connection_adapters/abstract_adapter.rb
@@ -34,20 +34,20 @@ module SchemaPlus
               super env.table_name, env.options
             end
           end
+        end
 
-          module SchemaCreation
+        module SchemaCreation
 
-            def add_column_options!(sql, options)
-              sql << " " + SchemaMonkey::Middleware::Sql::ColumnOptions.start(caller: self, connection: self.instance_variable_get('@conn'), sql: "", column: options[:column], options: options.except(:column)) { |env|
-                super env.sql, env.options.merge(column: env.column)
-              }.sql.lstrip
-            end
+          def add_column_options!(sql, options)
+            sql << " " + SchemaMonkey::Middleware::Sql::ColumnOptions.start(caller: self, connection: self.instance_variable_get('@conn'), sql: "", column: options[:column], options: options.except(:column)) { |env|
+              super env.sql, env.options.merge(column: env.column)
+            }.sql.lstrip
+          end
 
-            def visit_TableDefinition(o)
-              SchemaMonkey::Middleware::Sql::Table.start(caller: self, connection: self.instance_variable_get('@conn'), table_definition: o, sql: SqlStruct::Table.new) { |env|
-                env.sql.parse! super env.table_definition
-              }.sql.assemble
-            end
+          def visit_TableDefinition(o)
+            SchemaMonkey::Middleware::Sql::Table.start(caller: self, connection: self.instance_variable_get('@conn'), table_definition: o, sql: SqlStruct::Table.new) { |env|
+              env.sql.parse! super env.table_definition
+            }.sql.assemble
           end
         end
       end
